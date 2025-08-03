edd3ce524d75766b08346cf50b48447f15d6d41c
Nanda-maker <nandakrgurung123@gmail.com>
2025-08-03 19:35:37 +0600

    simplifying test setup

--- test/components/Greet.test.tsx
+++ test/components/Greet.test.tsx
@@ -1,18 +1,14 @@
-import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Greet from "../../src/components/Greet";
-import "@testing-library/jest-dom/vitest"; // for jest-dom matchers
 
 describe("Greet", () => {
   it("should render Hello with name when name is provided", () => {
-    // First we have render the greet component
     render(<Greet name="John" />);
     const heading = screen.getByRole("heading");
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent(/John/i);
   });
   it("should render login button when name is not provided", () => {
-    // First we have to render the greet component
     render(<Greet />);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument();
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    // globals: true,
     environment: "jsdom",
-    // setupFiles: './vitest.setup.ts',
+    globals: true,
+    setupFiles: "./test/setup.ts",
     // coverage: {
     // provider: 'v8',
     // reporter: ['text', 'json', 'html'],
